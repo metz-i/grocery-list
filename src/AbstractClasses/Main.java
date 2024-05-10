d2d1bb207dc6ab1e4e5edbcb42f5912188ff6ea7
Mutsuki Imai <mimai@catalyte.io>
2024-05-10 15:24:16 -0400

    Added items to list

--- src/AbstractClasses/Main.java
+++ src/AbstractClasses/Main.java
@@ -1,11 +1,19 @@
 package AbstractClasses;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         Dog dog = new Dog("Wolf", "big", 100);
         dog.makeNoise();
+        doAnimalStuff(dog);
 
+        ArrayList<Animal> animals = new ArrayList<>();
+        animals.add(dog);
+        animals.add(new Dog("Yorkie", "small", 13));
+        animals.add(new Fish("Goldfish", "small", 1.1));
+        animals.add(new Fish("Shark", "medium", 125));
     }
 
     private static void doAnimalStuff(Animal animal) {
